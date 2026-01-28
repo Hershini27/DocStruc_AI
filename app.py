11ca6c661c961485771fce22674a97cc9472f397
Hershini <hershiniuppununthala@gmail.com>
2026-01-29 00:22:20 +0530

    Started with preprocessing not done completely

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,request
+import re
 app=Flask(__name__)
 @app.route('/extract',methods=["POST"])
 def extract():
@@ -13,7 +14,7 @@ def extract():
             missing_data.append("schema")
         if missing_data:
             return {"error":f"Missing required fields:{','.join(missing_data)}"},400
-        if not isinstance(data["document"], str) or not data["document"].strip():
+        if not isinstance(data["document"], dict) or not data["document"]:
             return {"error": "'document' must be a non-empty string."}, 400
         if not isinstance(data["schema"], dict) or not data["schema"]:
             return {"error": "'schema' must be a non-empty JSON object."}, 400
@@ -22,5 +23,22 @@ def extract():
         return {"error": "Internal server error during validation."}, 500
     else:
         return data,200
+    
+def pre_process(data):
+    for key,value in data["document"].items():
+        if isinstance(value,str):
+            changed=value.strip()
+            regexed=re.sub(r'\s+',' ',changed)
+            unwanted=['$','₹','€',',']
+            wanted=regexed
+            for i in unwanted:
+                if i in wanted:
+                    wanted=wanted.replace(i,'')
+            data["document"][key]=wanted
+            
+            if re.search([1-9],value):
+                data["document"][key]=float(value)
+    return data
+
 if __name__=="__main__":
     app.run(debug=True)
